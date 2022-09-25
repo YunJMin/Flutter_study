0ab09ba99d5411cc7ee3db2414665cc2ddeec3f7
Jimin <101105251+YunJMin@users.noreply.github.com>
2022-09-25 13:12:12 +0900

    stateful

--- Stateful_widget/README.md
+++ Stateful_widget/README.md
@@ -14,3 +14,16 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+state란 UI가 변경되도록 영향을 미치는 데이터
+App 수준과 Widget 수준의 데이터가 있음
+
+Stateless widget
+ - State가 변하지 않는 위젯
+   Text('Korea') => Text('France)
+
+Widget tree and Element tree
+ - Reload vs.Rebuild
+ - Are the element tree and render tree rebuilt?
+ - Stateless 위젯은 rebuild만을 통해서 새오룬 State 적용 가능
+ - Flutter는 초당 60프레임 속도
--- Stateful_widget/lib/main.dart
+++ Stateful_widget/lib/main.dart
@@ -1,47 +1,63 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   int counter = 0;
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('You have pushed the butoon this many times: '),
+              Text('You have pushed the button this many times:'),
               Text(
                 '$counter',
-                style: Theme.of(context).textTheme.display1,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                      child: Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          counter++;
+                          print("$counter");
+                        });
+                      }),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  FloatingActionButton(
+                      child: Icon(Icons.remove),
+                      onPressed: () {
+                        setState(() {
+                          counter--;
+                          print("$counter");
+                        });
+                      }),
+                ],
               )
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: (){
-            counter++;
-            print("$counter");
-          },
         ),
       ),
     );
   }
 }
-// state란 UI가 변경되도록 영향을 미치는 데이터이다.
-// App 수준과 Widget 수준의 데이터가 있다.
-// Stateless widget
-//  - State가 변하지 않는 위젯
-//    Text('Korea') => Text('France)
-
-// Widget tree and Element tree
-//  - Reload vs.Rebuild
-//  - Are the element tree and render tree rebuilt?
-//  - Stateless 위젯은 rebuild만을 통해서 새오룬 State 적용 가능
-//  - Flutter는 초당 60프레임 속도
